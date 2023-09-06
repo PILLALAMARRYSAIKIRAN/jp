2dfdc7c5435834e92e9600e15f0f90bcfaa6fcf4
PILLALAMARRYSAIKIRAN <143397122+PILLALAMARRYSAIKIRAN@users.noreply.github.com>
2023-09-06 21:34:18 +0530

    Update 3d.java

--- 3d.java
+++ 3d.java
@@ -12,3 +12,16 @@ class Demo_3dArr {
 }
 }
 }
+    int[][][] arr = {{{1,2},{3,4}},{{2,4},{5,6}}};
+      
+      for(int i= 0;i<2;i++){
+           for(int j= 0;j<2;j++){
+                for(int k=0;k<2;k++){
+          
+        System.o
+
+
+
+
+
+
